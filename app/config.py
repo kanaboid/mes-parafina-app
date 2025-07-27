624218e282a6c2cfc90b9376b8e424b249ed5265
kanaboid <kanaboid@gmail.com>
2025-07-27 02:06:43 +0200

    Update database connection settings in config.py and modify requirements.txt

--- app/config.py
+++ app/config.py
@@ -2,12 +2,22 @@
 
 import os
 
+# class Config:
+#     # Klucz do zabezpieczeń Flaska, np. sesji. Na razie nieistotny, ale potrzebny.
+#     SECRET_KEY = os.environ.get('SECRET_KEY') or 'trudne-do-zgadniecia-haslo'
+
+#     # Dane do połączenia z bazą danych MySQL
+#     MYSQL_HOST = '10.200.184.217'
+#     MYSQL_USER = 'remote_user' # Zmień, jeśli masz innego użytkownika
+#     MYSQL_PASSWORD = 'Radar123@@' # <-- WAŻNE: Wpisz swoje hasło
+#     MYSQL_DB = 'mes_parafina_db'
+
 class Config:
     # Klucz do zabezpieczeń Flaska, np. sesji. Na razie nieistotny, ale potrzebny.
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'trudne-do-zgadniecia-haslo'
 
     # Dane do połączenia z bazą danych MySQL
-    MYSQL_HOST = '10.200.184.217'
-    MYSQL_USER = 'remote_user' # Zmień, jeśli masz innego użytkownika
-    MYSQL_PASSWORD = 'Radar123@@' # <-- WAŻNE: Wpisz swoje hasło
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root' # Zmień, jeśli masz innego użytkownika
+    MYSQL_PASSWORD = '' # <-- WAŻNE: Wpisz swoje hasło
     MYSQL_DB = 'mes_parafina_db'
